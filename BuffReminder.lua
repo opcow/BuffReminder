bc08c715fece5df22e55e37ff8909586f93b1ec8
Mitch Crane <mitch.crane@gmail.com>
2017-01-29 21:40:05 -0500

    bugs, conditonal icon showing

--- BuffReminder.lua
+++ BuffReminder.lua
@@ -1,31 +1,52 @@
 brBuffGroups = {}
 brOptions = {}
 brDefaultOptions = {
-    ["warntime"] = 60,
-    ["warnsound"] = nil,
+    ["conditions"] = {
+        ["resting"] = true,
+        ["taxi"] = true,
+        ["dead"] = true,
+        ["instance"] = false,
+        ["group"] = false,
+    },
     ["disabled"] = false,
-    ["size"] = 30
+    ["warnsound"] = nil,
+    ["size"] = 30,
+    ["warntime"] = 60,
+    ["alpha"] = 1.0,
 }
 
 local lbrLastBuffList = {}
 local lbrWarnIconFrames = {}
 local lbrUpdateTime = 0
-local lbrFirstRun = true
+local lbrForceUpdate = true
 local lbrButtonSpc = 2
+local lbrPlayerDead = false
+local lbrPlayerOnTaxi = false
+local lbrPlayerResting = false
+local lbrPlayerInInstance = false
 --------------------------------------------------------------------------------------------------
 function BuffReminder_OnLoad(Frame)
     
-    if brOptions.warntime == nil then brOptions = brDefaultOptions end
+    if brOptions == nil then brOptions = brDefaultOptions end
     this:RegisterForDrag("LeftButton")
     this:EnableMouse(false)
-    
+    -- this:RegisterEvent("PLAYER_ALIVE")
+    this:RegisterEvent("PLAYER_DEAD")
+    this:RegisterEvent("PLAYER_UNGHOST")
+    this:RegisterEvent("PLAYER_AURAS_CHANGED")
+    -- this:RegisterEvent("PLAYER_CONTROL_LOST")
+    -- this:RegisterEvent("PLAYER_CONTROL_GAINED")
+    this:RegisterEvent("PLAYER_ENTERING_WORLD")
+    this:RegisterEvent("UNIT_FLAGS")
+    this:RegisterEvent("PLAYER_UPDATE_RESTING")
+    this:RegisterEvent("PLAYER_ENTERING_WORLD")
+    --    this:RegisterAllEvents()
     -- tooltip frame for getting spell name
     lbrTooltipFrame = CreateFrame('GameTooltip', 'BrTooltip', UIParent, 'GameTooltipTemplate')
     lbrTooltipFrame:SetOwner(UIParent, 'ANCHOR_NONE')
     
     if (DEFAULT_CHAT_FRAME) then
         DEFAULT_CHAT_FRAME:AddMessage("BuffReminder AddOn loaded. Type '/br help' for config commands.")
-        DEFAULT_CHAT_FRAME:AddMessage("BuffReminder AddOn loaded")
     end
     UIErrorsFrame:AddMessage("BuffReminder AddOn loaded", 1.0, 1.0, 1.0, 1.0, UIERRORS_HOLD_TIME)
 
@@ -34,9 +55,9 @@ end
 function BuffReminder_OnUpdate(self)
     if GetTime() - lbrUpdateTime >= 1 then
         if brOptions.disabled then return end
-        if lbrFirstRun then
+        if lbrForceUpdate then
             BrChkBuffsExist()
-            lbrFirstRun = false
+            lbrForceUpdate = false
             BrDrawIcons()
         elseif BrChkBuffsExist() then
             BrDrawIcons()
@@ -44,13 +65,48 @@ function BuffReminder_OnUpdate(self)
         lbrUpdateTime = GetTime()
     end
 end
+
+function BuffReminder_OnEvent(event, arg1)
+    if event == "UNIT_FLAGS" and arg1 == "player" then
+        if UnitOnTaxi("player") == 1 then
+            lbrPlayerOnTaxi = true
+        else
+            lbrPlayerOnTaxi = false
+        end
+    elseif event == "PLAYER_DEAD" then
+        lbrPlayerDead = true
+    elseif event == "PLAYER_UNGHOST" then
+        lbrPlayerDead = false
+    elseif event == "PLAYER_UPDATE_RESTING" then
+        lbrPlayerResting = IsResting()
+    elseif event == "PLAYER_ENTERING_WORLD" then
+        lbrPlayerResting = IsResting()
+        lbrPlayerDead = UnitIsDeadOrGhost("player")
+        lbrPlayerOnTaxi = UnitOnTaxi("player") == 1
+        local isInstance, instanceType = IsInInstance()
+        lbrPlayerInInstance = isInstance
+    end
+    
+    
+    lbrForceUpdate = true -- force icon update
+end
 --------------------------------------------------------------------------------------------------
+function BrClearIcons()
+    for i in lbrWarnIconFrames do
+        lbrWarnIconFrames[i]:Hide()
+    end
+    lbrWarnIconFrames = {}
+end
+
 function BrDrawIcons()
     BrClearIcons()
-    if UnitIsDeadOrGhost("player") or UnitOnTaxi("player") then return end
-    
+
+    --if UnitIsDeadOrGhost("player") or UnitOnTaxi("player") then return end
     for i in brBuffGroups do
-        if brBuffGroups[i].show and brBuffGroups[i].enabled then
+        if (brBuffGroups[i].show and brBuffGroups[i].enabled) and not 
+        (lbrPlayerResting and brBuffGroups[i].conditions.resting) and not
+        (lbrPlayerInInstance and brBuffGroups[i].conditions.instance) and not
+        (lbrPlayerOnTaxi and brBuffGroups[i].conditions.taxi) then
             BrMakeIcon(brBuffGroups[i].icon)
         end
     end
@@ -58,10 +114,9 @@ function BrDrawIcons()
 end
 
 function GetPlayerBuffName(n)
-    MyScanningTooltip:ClearLines()
-    -- MyScanningTooltip:SetUnitBuff('player', n + 1)
-    MyScanningTooltip:SetPlayerBuff(n)
-    return MyScanningTooltipTextLeft1:GetText()
+    TooltipScanner:ClearLines()
+    TooltipScanner:SetPlayerBuff(n)
+    return TooltipScannerTextLeft1:GetText()
 end
 
 function BrGetPlayerBuffs()
@@ -87,7 +142,7 @@ function BrChkBuffsExist()
         for j in blist do
             if brBuffGroups[i].buffs[blist[j].name] ~= nil then
                 brBuffGroups[i].icon = blist[j].icon
-                if blist[j].time > brOptions.warntime then
+                if blist[j].time > brBuffGroups[i].warntime then
                     brBuffGroups[i].show = false
                     if shown then iconChanged = true end
                     break
@@ -109,15 +164,13 @@ function BrMakeIcon(icon)
     lbrWarnIconFrames[icon]:SetHeight(brOptions.size)
     local tex = lbrWarnIconFrames[icon]:CreateTexture(nil, "BACKGROUND")
     tex:SetTexture(icon)
+    -- if lbrPlayerDead or lbrPlayerOnTaxi or lbrPlayerResting then
+    --     tex:SetAlpha(1.0)
+    -- else
+        tex:SetAlpha(brOptions.alpha)
+    -- end
     tex:SetAllPoints(lbrWarnIconFrames[icon])
     lbrWarnIconFrames[icon].texture = tex
-end
-
-function BrClearIcons()
-    for i in lbrWarnIconFrames do
-        lbrWarnIconFrames[i]:Hide()
-    end
-    lbrWarnIconFrames = {}
 end
 
 function BrShowIcons()
@@ -131,17 +184,15 @@ function BrShowIcons()
 end
 
 function BrListGroups()
-    DEFAULT_CHAT_FRAME:AddMessage("\124cff00ff00\124h** buff groups **")
-    local n = 1
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffafe7e9\124h** buff groups **")
     for i in brBuffGroups do
-        DEFAULT_CHAT_FRAME:AddMessage("\124cff00ff00\124h" .. i)
+        DEFAULT_CHAT_FRAME:AddMessage("\124cffafe7e9\124h" .. i)
         local t = brBuffGroups[i].buffs
         for j in t do
-            DEFAULT_CHAT_FRAME:AddMessage("\124cff00ff00\124h  [" .. n .. "] " .. t[j].name)
-            n = n + 1
-        end
+            --            DEFAULT_CHAT_FRAME:AddMessage("\124cffafe7e9\124h  [" .. n .. "] " .. t[j].name)
+            end
     end
-    DEFAULT_CHAT_FRAME:AddMessage("\124cff00ff00\124h** end of buff groups **")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffafe7e9\124h** end of buff groups **")
 end
 
 function BrGetArgs(m)
@@ -170,26 +221,28 @@ end
 
 function BrAddBuffToGroup(name, grp)
     if brBuffGroups[grp] == nil then
-        brBuffGroups[grp] = {["icon"] = "Interface\\Icons\\INV_Misc_QuestionMark", ["enabled"] = true, ["warntime"] = brOptions.warntime, ["show"] = false, ["buffs"] = {}}
+        brBuffGroups[grp] = {["conditions"] = brOptions.conditions, ["warntime"] = brOptions.warntime, ["icon"] = "Interface\\Icons\\INV_Misc_QuestionMark", ["enabled"] = true, ["show"] = false, ["buffs"] = {}}
     end
     brBuffGroups[grp].buffs[name] = {}
-    DEFAULT_CHAT_FRAME:AddMessage("Added " .. name .. " to ''" .. grp .. "'.")
+    DEFAULT_CHAT_FRAME:AddMessage('Added "' .. name .. '" to "' .. grp .. '."')
 end
 
 function BrShowHelp()
-    DEFAULT_CHAT_FRAME:AddMessage("\124cff00ff00\124h ***** BuffReminder Help ***** ")
-    DEFAULT_CHAT_FRAME:AddMessage("\124cff00ff00\124hBuffs you want to monitor must be added to buff groups.")
-    DEFAULT_CHAT_FRAME:AddMessage("\124cff00ff00\124hMutually exclusive buffs should go into common groups.")
-    DEFAULT_CHAT_FRAME:AddMessage("\124cffffff00\124h/br add <buff> <group> \124cff00ff00\- adds a buff to a group.")
-    DEFAULT_CHAT_FRAME:AddMessage("\124cffffff00\124h/br disable <group> \124cff00ff00\- temporarily disable a buff group.")
-    DEFAULT_CHAT_FRAME:AddMessage("\124cffffff00\124h/br enable <group> \124cff00ff00\- enable a previously disabled group.")
-    DEFAULT_CHAT_FRAME:AddMessage("\124cffffff00\124h/br list \124cff00ff00\- shows your configured groups and watched buffs.")
-    DEFAULT_CHAT_FRAME:AddMessage("\124cffffff00\124h/br [lock|unlock] \124cff00ff00\- locks or unlocks the icon frame for user placement.")
-    DEFAULT_CHAT_FRAME:AddMessage("\124cffffff00\124h/br NUKE \124cff00ff00\- clears all of your groups.")
-    DEFAULT_CHAT_FRAME:AddMessage("\124cffffff00\124h/br remove buff <buff> \124cff00ff00\- removes a buff from being watched.")
-    DEFAULT_CHAT_FRAME:AddMessage("\124cffffff00\124h/br remove group <group> \124cff00ff00\- removes a buff group.")
-    DEFAULT_CHAT_FRAME:AddMessage("\124cffffff00\124h/br sound [sound name]\124cff00ff00\- sets the warning sound or turns it off if no name given. ex: /br sound RaidWarning")
-    DEFAULT_CHAT_FRAME:AddMessage("\124cff00ff00\124h ***** End of BuffReminder Help *****\124h\124r")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffafe7e9\124h ***** BuffReminder Help ***** ")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffafe7e9\124hBuffs you want to monitor must be added to buff groups.")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffafe7e9\124hMutually exclusive buffs should go into common groups.")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffbfb56f\124h/br add <buff> <group> \124cffafe7e9\- adds a buff to a group.")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffbfb56f\124h/br alpha <number> \124cffafe7e9\- change the icon transparency (min 0.0, max 1.0).")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffbfb56f\124h/br disable <group> \124cffafe7e9\- temporarily disable a buff group.")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffbfb56f\124h/br enable <group> \124cffafe7e9\- enable a previously disabled group.")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffbfb56f\124h/br list \124cffafe7e9\- shows your configured groups and watched buffs.")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffbfb56f\124h/br [lock|unlock] \124cffafe7e9\- locks or unlocks the icon frame for user placement.")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffbfb56f\124h/br NUKE \124cffafe7e9\- clears all of your settings.")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffbfb56f\124h/br remove buff <buff> \124cffafe7e9\- removes a buff from being watched.")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffbfb56f\124h/br remove group <group> \124cffafe7e9\- removes a buff group.")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffbfb56f\124h/br size <number> \124cffafe7e9\- change the icon size (min 10, max 400).")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffbfb56f\124h/br sound [sound name]\124cffafe7e9\- sets the warning sound or turns it off if no name given. ex: /br sound RaidWarning")
+    DEFAULT_CHAT_FRAME:AddMessage("\124cffafe7e9\124h ***** End of BuffReminder Help *****\124h\124r")
 end
 
 SLASH_BuffReminder1 = "/br" -- we add the slash commands
@@ -236,11 +289,18 @@ function SlashCmdList.BuffReminder(msg, editbox)-- we put the slash commands to 
             end
         elseif string.lower(args[1]) == "sound" then
             brOptions.warnsound = args[2]
+            PlaySound(tostring(brOptions.warnsound), "master")
             handled = true
         elseif string.lower(args[1]) == "size" then
             local n = brtonum(args[2])
-            if n ~= nil and (n >= 10 and n <= 100) then
+            if n ~= nil and (n >= 10 and n <= 400) then
                 brOptions.size = n
+                handled = true
+            end
+        elseif string.lower(args[1]) == "alpha" then
+            local n = brtonum(args[2])
+            if n ~= nil and (n >= 0 and n <= 1.0) then
+                brOptions.alpha = n
                 handled = true
             end
         elseif string.lower(args[1]) == "time" then
@@ -267,11 +327,23 @@ function SlashCmdList.BuffReminder(msg, editbox)-- we put the slash commands to 
                 end
                 handled = true
             end
+        elseif string.lower(args[1]) == "gtime" then
+            if brBuffGroups[args[2]] == nil then
+                DEFAULT_CHAT_FRAME:AddMessage('Group "' .. tostring(args[2]) .. '" does not exist.')
+                return
+            end
+            local n = brtonum(args[3])
+            if n ~= nil then
+                brBuffGroups[args[2]].warntime = n
+                handled = true
+            end
         end -- argc == 3
     end
     
+    lbrForceUpdate = true
+    
     if not handled then
-        DEFAULT_CHAT_FRAME:AddMessage("\124cffffff00\124hBuffReminder command error. Try /br help.")
+        DEFAULT_CHAT_FRAME:AddMessage("\124cffbfb56f\124hBuffReminder command error. Try /br help.")
     end
 end
 
